b7843b6ec2b0b19b9cc1815fc25402a637ac4672
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-07-08 11:56:49 +0300

    up

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename="delivery">
+      <BrowserRouter basename="delivery-app">
         <App />
       </BrowserRouter>
   </React.StrictMode>
